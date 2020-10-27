4a21184fa8235f635e4ceb59c414462955db6e39
code-bebop <woonj2@gmail.com>
2020-10-27 14:02:19 +0900

    MovieListItem의 style 설정

--- src/App.js
+++ src/App.js
@@ -11,23 +11,48 @@ const Wrapper = styled.div`
 const SearchMovieForm = styled.form`
   margin-top: 60px;
   text-align: center;
-  color: #fc3ce9;
+  & > input {
+    color: white;
+    background-color: transparent;
+    border: none;
+    border-bottom: 5px solid #fff;
+    outline: none;
+    min-width: 338px;
+  }
+  & > button {
+    display: none;
+  }
+`;
+
+const MovieMessage = styled.p`
+  color: #fff;
+  font-size: 3rem;
 `;
 
 const App = () => {
   const [items, setItems] = useState(null);
   const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const getData = useCallback(
     async (e) => {
       e.preventDefault();
+
+      if (query === "") {
+        alert("검색어를 입력해주십시오.");
+        return;
+      }
+
       try {
+        setLoading(true);
         const {
           data: { items: data },
         } = await getMovieList(query);
         setItems(data);
+        setLoading(false);
       } catch (e) {
         console.log(e);
+        setLoading(false);
       }
     },
     [query]
@@ -37,15 +62,19 @@ const App = () => {
     setQuery(e.target.value);
   };
 
-  console.log(query);
-
   return (
     <Wrapper>
       <SearchMovieForm onSubmit={getData}>
         <input onChange={onInputChange} value={query} />
         <button type="submit">검색</button>
       </SearchMovieForm>
-      {items ? <MovieList items={items} /> : <p>loading...</p>}
+      {!loading && items ? (
+        <MovieList items={items} />
+      ) : !items ? (
+        <MovieMessage>검색을 해 주십시오</MovieMessage>
+      ) : (
+        <MovieMessage>로딩 중 . . .</MovieMessage>
+      )}
     </Wrapper>
   );
 };
--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -3,6 +3,52 @@ import styled from "styled-components";
 
 const MovieListItemBlock = styled.li`
   color: #fff;
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 1196px;
+  height: 450px;
+  margin-top: 86px;
+  padding: 45px 45px;
+  & .Wrapper {
+    position: relative;
+    display: flex;
+    & > img {
+      max-width: 320px;
+      height: 360px;
+      margin-right: 45px;
+      flex: 2;
+    }
+    & > div {
+      flex: 1;
+      & > h3 {
+        font-size: 42px;
+        margin-bottom: 30px;
+      }
+      & > dl {
+        & > dt,
+        & > dd {
+          font-size: 18px;
+        }
+        & > dt {
+          display: block;
+          float: left;
+          width: auto;
+          margin: 0 40px 25px 0;
+          font-weight: bold;
+        }
+        & > dd {
+          padding-left: 100px;
+          &:after {
+            content: "";
+            display: block;
+            clear: both;
+          }
+        }
+      }
+    }
+  }
+  & + & {
+    margin-top: 57px;
+  }
 `;
 
 const MovieListItem = ({
@@ -15,21 +61,25 @@ const MovieListItem = ({
 }) => {
   return (
     <MovieListItemBlock>
-      <img
-        src={image}
-        alt={title.replace(/<b>/gi, "").replace(/<\/b>/gi, "")}
-      />
-      <h3>{title.replace(/<b>/gi, "").replace(/<\/b>/gi, "")}</h3>
-      <dl>
-        <dt>평점</dt>
-        <dd>{userRating}</dd>
-        <dt>개봉일</dt>
-        <dd>{pubDate}</dd>
-        <dt>감독</dt>
-        <dd>{director}</dd>
-        <dt>배우</dt>
-        <dd>{actor}</dd>
-      </dl>
+      <div className="Wrapper">
+        <img
+          src={image}
+          alt={title.replace(/<b>/gi, "").replace(/<\/b>/gi, "")}
+        />
+        <div>
+          <h3>{title.replace(/<b>/gi, "").replace(/<\/b>/gi, "")}</h3>
+          <dl>
+            <dt>평점</dt>
+            <dd>{userRating}</dd>
+            <dt>개봉일</dt>
+            <dd>{pubDate}</dd>
+            <dt>감독</dt>
+            <dd>{director}</dd>
+            <dt>배우</dt>
+            <dd>{actor}</dd>
+          </dl>
+        </div>
+      </div>
     </MovieListItemBlock>
   );
 };
