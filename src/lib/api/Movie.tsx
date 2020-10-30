b584dc884ab20a9952f67b2df5a72cc8c3d2d68b
code-bebop <woonj2@gmail.com>
2020-10-30 20:05:41 +0900

    API를 요청할 때에 Header에 같이 넣어준 ScretKey를 환경변수로 분리해서 gitignore처리함

--- src/lib/api/Movie.tsx
+++ src/lib/api/Movie.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const getMovieList = async (query: string, display: number) =>
   await axios.get(`/v1/search/movie.json?query=${query}&display=${display}`, {
     headers: {
       "X-Naver-Client-Id": "VWn8d_Kescc7tCMaYjkx",
-      "X-Naver-Client-Secret": "YOvbZD8FiW",
+      "X-Naver-Client-Secret": process.env.X_Naver_Client_Secret,
     },
   });
